e6c07405171cd5ed837c287d2dd123d17e9ae8b6
pisonmison <58531355+pisonmison@users.noreply.github.com>
2020-06-18 19:31:55 +0200

    Version 2

--- app/src/main/java/com/example/multitouch/MultiTouchGameScreen.java
+++ app/src/main/java/com/example/multitouch/MultiTouchGameScreen.java
@@ -3,12 +3,88 @@ package com.example.multitouch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
-public class MultiTouchGameScreen extends AppCompatActivity {
+public class MultiTouchGameScreen extends AppCompatActivity implements Button.OnClickListener, View.OnTouchListener {
+
+
+    private FrameLayout touchFrame;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multi_touch_game_screen);
+
+        touchFrame = findViewById(R.id.touchFrame_ID);
+        touchFrame.setOnTouchListener(this);
+
+
+
+    }
+
+
+
+
+
+
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+
+
+    private View.OnTouchListener handleTouch = new View.OnTouchListener() {
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+
+            int x = (int) event.getX();
+            int y = (int) event.getY();
+
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    Log.i("TAG", "touched down");
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    Log.i("TAG", "moving: (" + x + ", " + y + ")");
+                    break;
+                case MotionEvent.ACTION_UP:
+                    Log.i("TAG", "touched up");
+                    break;
+            }
+
+            return true;
+        }
+    };
+
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Log.i("TAG", "touched down");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.i("TAG", "moving: (" + x + ", " + y + ")");
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.i("TAG", "touched up");
+                break;
+        }
+
+
+
+        return true;
     }
 }
